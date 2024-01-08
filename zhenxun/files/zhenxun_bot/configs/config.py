8867c366fdad7e112ff93a78f5ffb20398073171
Fang_Zhijian <klxf@vip.qq.com>
2024-01-08 13:03:43 +0800

    Fix db error and playwright error
    阿巴阿巴

--- zhenxun/files/zhenxun_bot/configs/config.py
+++ zhenxun/files/zhenxun_bot/configs/config.py
@@ -10,7 +10,7 @@ NICKNAME: str = "小真寻"
 # 如果填写了bind就不需要再填写后面的字段了#）
 # 示例："bind": "postgresql://user:password@127.0.0.1:5432/database"
 bind: str = ""  # 数据库连接链接
-sql_name: str = "postgresql"
+sql_name: str = "postgres"
 user: str = "zhenxun"
 password: str = "123456"
 address: str = "postgres"
